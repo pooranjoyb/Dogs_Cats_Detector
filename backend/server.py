653ebd69237f4d5635376682d5968c3e42454d1c
pooranjoyb <pooranjoyb2016@gmail.com>
2023-04-28 12:07:21 +0530

    Routing complete

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @app.route("/")
 def home():
@@ -13,7 +13,7 @@ def home():
 def upload():
     file = request.files['file']
     file.save('uploads/' + file.filename)
-    return 'File uploaded successfully'
+    return jsonify({"message": "The ML prediction returned"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState("hello from react");
+  const [val, setVal] = useState("Upload image to predict")
 
   useEffect(() => {
     fetch("http://localhost:5000").then(
@@ -14,15 +15,39 @@ function App() {
     }
     )
   }, [])
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      axios.post('http://localhost:5000/upload', formData).then(
+        (res) =>{
+          console.log(res.data.message)
+          setVal(res.data.message)
+        }
+      );
+      alert('File uploaded successfully');
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       {data}
-    <form action="http://localhost:5000/upload" method="POST" encType="multipart/form-data">
+    <form onSubmit={handleSubmit}>
       
       <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
-      <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" name="file" />
+      <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" name="file" onChange={(e) => setFile(e.target.files[0])} />
       <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
     <button className="btn-primary" type="submit">Upload</button>
+    <br/>
+    {val}
     </form>
     </>
   );
